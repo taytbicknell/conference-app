854f69009583cf570194462128b2cf951660b14e
taytbicknell <taytbicknell@me.com>
2018-09-11 21:19:44 -0700

    adds meeting validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,13 @@
 class Meeting < ApplicationRecord
-  validates :title, presence: true
-  validates :agenda, presence: true
-  validates :time, presence: true
+  validates :title, length: {minimum: 2}
+  validates :title, length:  {maximum: 40}
+  validates :agenda, length: {maximum: 40} 
+  validates :agenda, length: {minimum: 2}
+  validates :agenda, length: {maximum: 40} 
 
+  validates :time, presence: true
+  validates :time, numericality: true
+  
   def friendly_created_at
     created_at.strftime("%A, %b %d")
   end 
